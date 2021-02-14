c9938475625ee7a7fef5851760735156f98448d1
Julian <juleig@web.de>
2021-02-14 11:17:55 +0100

    add basic authentification

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import ElementsOverview from './ElementsOverview';
+import ManageGymnasts from './ManageGymnasts';
 import Header from './Header';
 import Home from './Home';
+import { withAuthenticator } from '@aws-amplify/ui-react';
 
 const App = () => {
   return (
@@ -12,10 +14,11 @@ const App = () => {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/elements" exact component={ElementsOverview} />
+          <Route path="/gymnasts" exact component={ManageGymnasts} />
         </Switch>
       </Router>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,23 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
+import { AmplifySignOut } from '@aws-amplify/ui-react';
 
 const Header = () => {
     const [active, setActive] = useState("home")
     return(
-        <div className="ui secondary pointing menu">
-            <Link name="home" className={`item ${active === "home" ? 'active' : null}`} to="/" onClick={(e) => setActive(e.target.name)}>
-                Home
-            </Link>
-            <Link name="cdp" className={`item ${active === "cdp" ? 'active' : null}`} to="/elements" onClick={(e) => setActive(e.target.name)}>
-                Code de Pointage Elements
-            </Link>           
+        <div>
+            <div className="ui secondary pointing menu">
+                <Link className={`item ${active === "home" ? 'active' : null}`} to="/" onClick={(e) => setActive("home")}>
+                    Home
+                </Link>
+                <Link className={`item ${active === "cdp" ? 'active' : null}`} to="/elements" onClick={(e) => setActive("cdp")}>
+                    Code de Pointage Elements
+                </Link>
+                <Link className={`item ${active === "gymnasts" ? 'active' : null}`} to="/gymnasts" onClick={(e) => setActive("gymnasts")}>
+                    Manage Gymnasts
+                </Link>        
+            </div>
+            <AmplifySignOut/>
         </div>
     )
 }
